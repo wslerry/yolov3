8664372aa053c699fc7428ff30ba1e6bd52b8a00
wslerry <wslerry2@hotmail.com>
2020-06-18 13:06:12 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -6,7 +6,7 @@ from utils.utils import *
 
 
 def detect(save_img=False):
-    imgsz = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
+    imgsz = (416, 256) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
     out, source, weights, half, view_img, save_txt = opt.output, opt.source, opt.weights, opt.half, opt.view_img, opt.save_txt
     webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
 
