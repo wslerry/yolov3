bf9ccccd8cd9a5db1869bc8540fb0db94b8119be
wslerry <wslerry2@hotmail.com>
2020-06-19 14:10:24 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -124,6 +124,7 @@ def detect(save_img=False):
                 for *xyxy, conf, cls in det:
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        print(xywh)
                         with open(save_path[:save_path.rfind('.')] + '.txt', 'a') as file:
                             file.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
 
